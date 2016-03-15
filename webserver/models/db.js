40ec4205304777db9f3b040adbb83521a51c1ca1
amithubgit <321amitsharma@gmail.com>
2016-03-15 12:05:29 +0530

    update

--- webserver/models/db.js
+++ webserver/models/db.js
@@ -1,18 +1,18 @@
 module.exports={
                  pg_migrate: function pg_migrate(){
-                    var client=this.pg_connect();
-                    client.query("create table login (uname char(20), pass char(20))"); 	         
+                       var client=this.pg_connect();
+                       client.query("create table login (uname char(20), pass char(20))"); 	         
             },
 
-         pg_connect: function pg_connect() {
+                pg_connect: function pg_connect() {
 
                   var pg = require('pg');
-	          var connectionString = "postgres://postgres@localhost:5432/employee";
+                  var connectionString = "postgres://postgres@localhost:5432/employee";               
 	          var client = new pg.Client(connectionString);
 	          client.on('drain', client.end.bind(client)); //disconnect client when all queries are finished
 	          client.connect();
 	          return client;
-   }
-
+   },
+          
 }
 	
