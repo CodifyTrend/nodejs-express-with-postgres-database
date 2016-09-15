b7a1684e6b3e2a4bf12cd8ef582f02ddcfd4a73a
a1sharma <Amit.Sharma@safenet-inc.com>
2016-09-15 11:03:22 +0530

    added angularjs page

--- webserver/models/db.js
+++ webserver/models/db.js
@@ -1,7 +1,10 @@
 module.exports={
                  pg_migrate: function pg_migrate(){
                        var client=this.pg_connect();
-                    client.query("create table login (uname char(20), pass char(20))"); 	         
+                    client.query("CREATE TABLE IF NOT EXISTS login (uname char(20), pass char(20))"); 
+      client.query("CREATE TABLE IF NOT EXISTS item(id SERIAL PRIMARY KEY, iname VARCHAR(60))");	  
+client.query("CREATE TABLE IF NOT EXISTS vendor(id SERIAL PRIMARY KEY, vname VARCHAR(60))");	   
+client.query(" CREATE TABLE IF NOT EXISTS record(id SERIAL PRIMARY KEY, item VARCHAR(60), vendor VARCHAR(60),store VARCHAR(60))");	          
             },
 
                 pg_connect: function pg_connect() {
--- webserver/public/javascripts/global.js
+++ webserver/public/javascripts/global.js
@@ -1,52 +1,68 @@
-function insert(){
- 
-    var u = $("#uname").val();
-    var p = $("#pass").val();
-    $.ajax({
-        type: "POST",
-        url: "/insert",
-        dataType: "json",
-        encode: true,
-        contentType: 'application/json',
-        data: JSON.stringify({username:u,password:p}),
-        success: function(msg){   
-           console.log("succ", msg); 
-           alert("registration success");
-           window.location.href='/login';
-        },
-        error: function (error) {    
-            console.log("error", error); 
-        }
-     });
+var app = angular.module('myApp', []);
+app.controller('ExampleController', ['$scope', function($scope) {
 
-}
+   $scope.data = {
 
+    singleSelect:null,
 
-function populateTable(){
-    var u = $("#uname").val();
-    var p = $("#pass").val();
-    $.ajax({
-        type: "POST",
-        url: "/send",
-        dataType: "json",
-        encode: true,
-        contentType: 'application/json',
-        data: JSON.stringify({username:u,password:p}),
-        success: function(msg){
-            console.log("succ", msg);
-            if(msg.message=="true"){
-             window.location.href='/alert';
-            } 
-            else {
-           alert("username and password is incorrect");
-               }
-             
-        },
-        error: function (error) {
-        
-            console.log("error", error);
-            
-        }
-     });
+    multipleSelect: [],
 
-}
+    option1: 'option-1'
+
+   };
+}]);
+
+app.controller('DateController', ['$scope', function($scope) {
+        $scope.requestDate = new Date(2014, 3, 19);
+    }
+]);
+app.controller('DateController', ['$scope', function($scope) {
+      $scope.example = {
+        value: new Date(2010, 11, 28, 14, 57)
+      };
+    }]);
+
+app.controller('myCtrl', ['$scope', function($scope) {
+
+$scope.checkboxModel = {
+     };
+
+$scope.selectAll = function() {
+console.log("inside selectAll func");
+$scope.checkboxModel.value1 = true;
+$scope.checkboxModel.value2 = true;
+$scope.checkboxModel.value3 = true;
+$scope.checkboxModel.value4 = true;
+$scope.checkboxModel.value5 = true;
+$scope.checkboxModel.value6 = true;
+      
+};    
+$scope.deselectAll = function() {
+console.log("inside deselectAll func");
+$scope.checkboxModel.value1 = false;
+$scope.checkboxModel.value2 = false;
+$scope.checkboxModel.value3 = false;
+$scope.checkboxModel.value4 = false;
+$scope.checkboxModel.value5 = false;
+$scope.checkboxModel.value6 = false;
+      
+};          
+
+$scope.saveAll = function () {
+		console.log("inside saveall func");
+                    data = {}
+                    $http.get("/", data
+                        ).success(function (res, status, headers, config) {
+                    if (res == 'success')
+                    {
+                        $scope.message = res;
+                    }
+                    else
+                    {
+                        $scope.message = res;
+                    }
+                }).error(function (res, status) { 
+                    $scope.message = res;
+                });
+                }
+    }]);
